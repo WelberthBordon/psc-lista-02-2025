496940154a2b0b84d36acf6c240a5716c7bf03dc
@WelberthBordon <joaowelberth751@gmail.com>
2025-04-24 08:31:31 -0300

    Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -4,14 +4,12 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita dois números ao usuário
         System.out.print("Digite o primeiro número: ");
         double num1 = scanner.nextDouble();
         
         System.out.print("Digite o segundo número: ");
         double num2 = scanner.nextDouble();
 
-        // Exibe os resultados das operações
         System.out.println("Soma: " + (num1 + num2));
         System.out.println("Subtração: " + (num1 - num2));
         System.out.println("Multiplicação: " + (num1 * num2));
